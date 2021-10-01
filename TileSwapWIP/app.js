8448e2388614a4f468f95b104caa2a16d87c8970
notArodnt <47108856+notArodnt@users.noreply.github.com>
2021-10-01 13:48:55 +0200

    updated endurance mode

--- TileSwapWIP/app.js
+++ TileSwapWIP/app.js
@@ -1328,7 +1328,7 @@ function selectChallenge(challenge) {
 
 function selectChallengeDifficulty(difficulty) {
   app.challenge.difficulty = difficulty;
-  app.challenge.baseMoves = [7, 40, 60, 100, -1][difficulty] * (app.challenge.baseTime === -1 ? 1 : app.challenge.baseTime/60) ;
+  app.challenge.baseMoves = [7, 40, 60, 100, -1][difficulty] * (app.challenge.baseTime === -1 ? 3 : app.challenge.baseTime/60) ;
   app.challenge.remainingMoves = app.challenge.baseMoves;
 
   app.openScreen('challenges');
