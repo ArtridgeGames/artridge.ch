4a0eab2fc811470645ccd81947b23aedaee2670a
Artridge <48982618+ArtridgeGames@users.noreply.github.com>
2021-10-24 21:43:43 +0200

    ajout partiel des details de la progression

--- TileSwapWIP/app.js
+++ TileSwapWIP/app.js
@@ -7,18 +7,22 @@ const layouts = [
   {
     dimensions: '3x3',
     exclude: []
+    //unlockCategory: 1
   },
   {
     dimensions: '4x4',
     exclude: [] 
+    //unlockCategory: 5
   },
   {
     dimensions: '5x5',
     exclude: [] 
+    //unlockCategory: 11
   },
   {
     dimensions: '6x6',
     exclude: [] 
+    //unlockCategory: 19
   },
   {
     dimensions: '7x7',
@@ -48,27 +52,33 @@ const layouts = [
   {
     dimensions: '3x4',
     exclude: [] 
+    //unlockCategory: 8
   },
   {
     dimensions: '3x5',
     exclude: [] 
+    //unlockCategory: 3
   },
   {
     dimensions: '3x6',
     exclude: [] 
+    //unlockCategory: 20
   },
   {
     dimensions: '3x7',
     exclude: [] 
+    //unlockCategory: 20
   },
   //diamond
   {
     dimensions: '3x3',
     exclude: [0,2,6,8] 
+    //unlockCategory: 2
   },
   {
     dimensions: '5x5',
     exclude: [0,1,3,4,5,9,15,19,20,21,23,24] 
+    //unlockCategory: 12
   },
   {
     dimensions: '7x7',
@@ -90,174 +100,217 @@ const layouts = [
   {
     dimensions: '3x3',
     exclude: [3,5,6,8] 
+    //unlockCategory: 2
   },
   {
     dimensions: '3x3',
     exclude: [4] 
+    //unlockCategory: 2
   },
   {
     dimensions: '3x3',
     exclude: [1,3,5,7] 
+    //unlockCategory: 2
   },
   {
     dimensions: '3x3',
     exclude: [4,7] 
+    //unlockCategory: 2
   },
   {
     dimensions: '4x4',
     exclude: [5,10] 
+    //unlockCategory: 9
   },
   {
     dimensions: '4x4',
     exclude: [3,12] 
+    //unlockCategory: 9
   },
   {
     dimensions: '4x4',
     exclude: [3,6,9,12] 
+    //unlockCategory: 8
   },
   {
     dimensions: '4x4',
     exclude: [0,3,12,15] 
+    //unlockCategory: 9
   },
   {
     dimensions: '4x4',
     exclude: [2,3,7,8,12,13] 
+    //unlockCategory: 7
   },
   {
     dimensions: '4x4',
     exclude: [0,3,6,8,12,13,15] 
+    //unlockCategory: 7
   },
   {
     dimensions: '4x4',
     exclude: [0,3,5,6,9,10,12,15] 
+    //unlockCategory: 6
   },
   {
     dimensions: '4x4',
     exclude: [0,1,4,5,10,11,14,15] 
+    //unlockCategory: 6
   },
   {
     dimensions: '4x4',
     exclude: [1,2,3,4,6,7,8,9,11,12,13,14] 
+    //unlockCategory: 6
   },
   {
     dimensions: '3x5',
     exclude: [0,2,12,14] 
+    //unlockCategory: 8
   },
   {
     dimensions: '5x2',
     exclude: [2,5,9] 
+    //unlockCategory: 6
   },
   {
     dimensions: '3x4',
     exclude: [4,6,8] 
+    //unlockCategory: 7
   },
   {
     dimensions: '5x5',
     exclude: [0,2,4,10,14,20,22,24] 
+    //unlockCategory: 14
   },
   {
     dimensions: '5x5',
     exclude: [0,2,4,10,12,14,20,22,24] 
+    //unlockCategory: 14
   },
   {
     dimensions: '5x5',
     exclude: [6,8,16,18] 
+    //unlockCategory: 13
   },
   {
     dimensions: '5x5',
     exclude: [6,8,11,13,16,18] 
+    //unlockCategory: 14
   },
   {
     dimensions: '5x5',
     exclude: [5,7,9,10,12,14,15,17,19] 
+    //unlockCategory: 14
   },
   {
     dimensions: '5x5',
     exclude: [0,2,4,7,10,11,13,14,17,20,22,24] 
+    //unlockCategory: 12
   },
   {
     dimensions: '5x5',
     exclude: [0,2,4,5,7,9,15,17,19,20,22,24] 
+    //unlockCategory: 12
   },
   {
     dimensions: '5x5',
     exclude: [0,4,5,6,8,9,15,16,18,19,20,24] 
+    //unlockCategory: 12
   },
   {
     dimensions: '5x5',
     exclude: [2,4,6,8,10,12,16,19,20,23,24] 
+    //unlockCategory: 17
   },
   {
     dimensions: '5x5',
     exclude: [0,1,5,6,7,9,10,14,15,17,18,19,23,24] 
+    //unlockCategory: 8
   },
   {
     dimensions: '3x3',
     exclude: [2,4,6] 
+    //unlockCategory: 2
   },
   {
     dimensions: '5x5',
     exclude: [0,4,20,24] 
+    //unlockCategory: 13
   },
   {
     dimensions: '5x5',
     exclude: [0,4,20,24,7,11,12,13,17] 
+    //unlockCategory: 14
   },
   {
     dimensions: '5x5',
     exclude: [2,10,14,22] 
+    //unlockCategory: 13
   },
   {
     dimensions: '5x5',
     exclude: [12]
+    //unlockCategory: 13
   },
   {
     dimensions: '5x5',
     exclude: [0,1,3,4,5,7,9,11,13,15,17,19,20,21,23,24] 
+    //unlockCategory: 10
   },
   {
     dimensions: '4x7',
     exclude: [1,2,3,6,7,9,11,13,14,17,19,22,23,25,26,27] 
+    //unlockCategory: 17
   },
   {
     dimensions: '5x5',
     exclude: [1,3,5,7,9,11,13,15,17,19,21,23] 
+    //unlockCategory: 10
   },
   {
     dimensions: '5x5',
     exclude: [0,2,4,6,8,10,12,14,16,18,20,22,24] 
+    //unlockCategory: 10
   },
   {
     dimensions: '5x5',
     exclude: [7,12,17] 
+    //unlockCategory: 13
   },
   {
     dimensions: '5x5',
     exclude: [7,11,13,17] 
+    //unlockCategory: 13
   },
   {
     dimensions: '5x5',
     exclude: [1,3,5,9,11,13,15,19,21,23] 
+    //unlockCategory: 14
   },
   {
     dimensions: '5x5',
     exclude: [1,2,3,7,17,21,22,23] 
+    //unlockCategory: 12
   },
   {
     dimensions: '5x5',
     exclude: [0,1,3,4,5,6,8,9,15,16,18,19,20,21,23,24] 
+    //unlockCategory: 7
   },
   {
     dimensions: '4x7',
     exclude: [0,1,2,4,5,8,16,20,21,24,25,26] 
+    //unlockCategory: 18
   },
   {
     dimensions: '5x5',
     exclude: [5,6,7,8,13,16,17,18] 
+    //unlockCategory: 18
   },
   {
     dimensions: '3x5',
     exclude: [0,2,4,10,12,14] 
+    //unlockCategory: 7
   },
   {
     dimensions: '7x7',
@@ -266,10 +319,12 @@ const layouts = [
   {
     dimensions: '5x4',
     exclude: [0,1,3,4,11,12,13,17] 
+    //unlockCategory: 15
   },
   {
     dimensions: '6x6',
     exclude: [0,1,3,4,5,6,10,11,12,13,15,17,18,20,27,29,30,32,33,34,35] 
+    //unlockCategory: 17
   },
   {
     dimensions: '7x7',
@@ -278,6 +333,7 @@ const layouts = [
   {
     dimensions: '5x5',
     exclude: [1,3,6,8,10,12,14,16,18,21,23] 
+    //unlockCategory: 10
   },
   {
     dimensions: '7x7',
@@ -286,14 +342,17 @@ const layouts = [
   {
     dimensions: '6x6',
     exclude: [0,1,2,3,4,9,10,13,14,19,21,22,23,24,25,27,28,29,33,34,35] 
+    //unlockCategory: 16
   },
   {
     dimensions: '5x4',
     exclude: [6,7,8,10,14] 
+    //unlockCategory: 18
   },
   {
     dimensions: '6x6',
     exclude: [0,1,3,4,5,6,10,11,15,17,18,20,24,25,29,30,31,32,34,35] 
+    //unlockCategory: 18
   },
   {
     dimensions: '5x7',
@@ -302,6 +361,7 @@ const layouts = [
   {
     dimensions: '3x8',
     exclude: [4,6,8,10,13,15,17,19] 
+    //unlockCategory: 18
   },
   {
     dimensions: '7x7',
@@ -322,6 +382,7 @@ const layouts = [
   {
     dimensions: '5x6',
     exclude: [2,6,8,11,12,13,16,17,18,21,23,27] 
+    //unlockCategory: 21
   },
   {
     dimensions: '7x9',
@@ -342,22 +403,27 @@ const layouts = [
   {
     dimensions: '4x6',
     exclude: [0,1,4,6,8,10,13,14,15,18,19,22,23] 
+    //unlockCategory: 8
   },
   {
     dimensions: '6x4',
     exclude: [2,3,6,7,10,11,18,20,21,23] 
+    //unlockCategory: 17
   },
   {
     dimensions: '5x5',
     exclude: [0,1,3,4,5,7,11,12,13,15,17,20,21,23,24] 
+    //unlockCategory: 7
   },
   {
     dimensions: '5x3',
     exclude: [0,1,2,5,8,11,13]
+    //unlockCategory: 6
   },
   {
     dimensions: '7x3',
     exclude: [1,5,15,19]
+    //unlockCategory: 18
   },
   {
     dimensions: '9x6',
@@ -382,22 +448,27 @@ const layouts = [
   {
     dimensions: '8x6',
     exclude: [0,1,2,3,4,6,7,12,15,17,18,19,22,24,25,28,29,30,32,33,36,38,39,40,41,42,43,45,46,47]
+    //unlockCategory: 21
   },
   {
     dimensions: '4x7',
     exclude: [0,3,5,7,8,10,11,12,15,16,19,20,21,22,24,27] 
+    //unlockCategory: 16
   },
   {
     dimensions: '7x5',
     exclude: [2,3,4,7,9,11,13,14,17,20,21,23,25,27,30,31,32] 
+    //unlockCategory: 21
   },
   {
     dimensions: '6x6',
     exclude: [0,1,3,4,5,6,7,9,10,11,16,17,18,19,24,25,26,28,29,30,31,32,34,35] 
+    //unlockCategory: 16
   },
   {
     dimensions: '6x6',
     exclude: [3,4,5,7,9,10,11,12,15,16,17,18,19,20,23,24,25,26,28,30,31,32,33] 
+    //unlockCategory: 17
   },
   {
     dimensions: '7x5',
@@ -406,22 +477,27 @@ const layouts = [
   {
     dimensions: '6x6',
     exclude: [0,4,5,7,9,11,14,19,21,22,24,27,28,29,30,31,34,35] 
+    //unlockCategory: 21
   },
   {
     dimensions: '5x4',
     exclude: [1,2,3,6,8] 
+    //unlockCategory: 14
   },
   {
     dimensions: '7x7',
     exclude: [0,3,4,5,6,8,9,11,12,13,14,16,18,19,20,21,22,24,27,28,29,30,32,33,35,36,37,38,40,42,43,44,45,46,48] 
+    //unlockCategory: 16
   },
   {
     dimensions: '5x6',
     exclude: [0,1,4,5,7,8,10,12,17,19,21,22,24,25,28,29] 
+    //unlockCategory: 17
   },
   {
     dimensions: '10x5',
     exclude: [0,1,2,3,4,5,10,11,12,13,14,16,17,18,19,20,21,22,24,26,27,28,29,33,35,36,37,38,39,40,41,42,43,45,46,47,48,49] 
+    //unlockCategory: 16
   },
   {
     dimensions: '10x6',
@@ -434,6 +510,7 @@ const layouts = [
   {
     dimensions: '6x3',
     exclude: [0,5,12,17]
+    //unlockCategory: 15
   },
   {
     dimensions: '7x7',
@@ -446,14 +523,17 @@ const layouts = [
   {
     dimensions: '3x5',
     exclude: [2,4,8,10,14] 
+    //unlockCategory: 3
   },
   {
     dimensions: '3x5',
     exclude: [0,4,5,7,8,10,11,12] 
+    //unlockCategory: 3
   },
   {
     dimensions: '3x5',
     exclude: [2,4,7,10,14] 
+    //unlockCategory: 3
   },
   {
     dimensions: '5x5',
@@ -478,14 +558,17 @@ const layouts = [
   {
     dimensions: '7x5',
     exclude: [1,2,3,4,5,6,7,10,11,12,13,14,21,22,23,25,27,28,29,30,32,34] 
+    //unlockCategory: 17
   },
   {
     dimensions: '5x8',
     exclude: [0,4,6,7,8,10,11,12,13,15,16,17,19,20,21,23,24,25,26,28,29,30,31,32,33,34,35,36,38,39] 
+    //unlockCategory: 15
   },
   {
     dimensions: '3x5',
     exclude: [0,2,4,10,13] 
+    //unlockCategory: 3
   },
   {
     dimensions: '7x9',
@@ -498,18 +581,22 @@ const layouts = [
   {
     dimensions: '8x3',
     exclude: [1,2,3,4,5,6,8,10,11,12,13,15,16,17,22,23]
+    //unlockCategory: 6
   },
   {
     dimensions: '3x4',
     exclude: [0,2,4,7,9,11]
+    //unlockCategory: 6
   },
   {
     dimensions: '3x4',
     exclude: [4,7]
+    //unlockCategory: 4
   },
   {
     dimensions: '7x7',
     exclude: [0,1,2,4,5,6,7,8,10,12,13,14,16,18,20,22,24,26,28,30,32,34,35,36,38,40,41,42,43,44,46,47,48]
+    //unlockCategory: 18
   },
   {
     dimensions: '9x9',
@@ -522,6 +609,7 @@ const layouts = [
   {
     dimensions: '3x5',
     exclude: [0,2,4]
+    //unlockCategory: 15
   },
   {
     dimensions: '4x7',
@@ -530,18 +618,22 @@ const layouts = [
   {
     dimensions: '3x5',
     exclude: [1,2,4,5,7,8,10,11]
+    //unlockCategory: 4
   },
   {
     dimensions: '3x5',
     exclude: [3,5,6,8,9,11]
+    //unlockCategory: 4
   },
   {
     dimensions: '5x5',
     exclude: [1,2,3,7,11,13,16,17,18,21,22,23]
+    //unlockCategory: 15
   },
   {
     dimensions: '5x5',
     exclude: [1,2,3,7,8,13,16,21,22]
+    //unlockCategory: 15
   },
   {
     dimensions: '7x7',
@@ -558,6 +650,7 @@ const layouts = [
   {
     dimensions: '5x5',
     exclude: [2,11,12,13,17]
+    //unlockCategory: 13
   },
   {
     dimensions: '9x5',
@@ -574,22 +667,27 @@ const layouts = [
   {
     dimensions: '3x5',
     exclude: [4,5,8,10,11]
+    //unlockCategory: 3
   },
   {
     dimensions: '3x5',
     exclude: [4,5,8,10,11,13,14]
+    //unlockCategory: 4
   },
   {
     dimensions: '3x5',
     exclude: [0,4,5,7,8,10,12]
+    //unlockCategory: 4
   },
   {
     dimensions: '3x5',
     exclude: [1,4,10,13]
+    //unlockCategory: 4
   },
   {
     dimensions: '3x5',
     exclude: [1,4,8,10,13]
+    //unlockCategory: 4
   },
   {
     dimensions: '11x8',
